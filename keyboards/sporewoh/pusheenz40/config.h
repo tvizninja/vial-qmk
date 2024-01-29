9a07d56eb8a259b32747f429a2de8a89826ccdc2
ChrisChrisLoLo <sporewoh@gmail.com>
2024-01-28 22:11:08 -0600

    add keyboard maps for modkipz40 and pusheenz40

---
+++ keyboards/sporewoh/pusheenz40/config.h
@@ -0,0 +1,43 @@
+/*
+Copyright 2023 sporewoh
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* USB Device descriptor parameter */
+// #define VENDOR_ID       0xBEAF
+// #define PRODUCT_ID      0x000A
+// #define DEVICE_VER      0x0000
+// #define MANUFACTURER    sporewoh
+// #define PRODUCT         bunchiez40
+
+/* key matrix size */
+// #define MATRIX_ROWS 4
+// #define MATRIX_COLS 10
+
+/* pin-out for PCB */
+// #define MATRIX_ROW_PINS { GP5, GP7, GP8, GP14 } // config for my borked board :((
+// #define MATRIX_ROW_PINS { GP6, GP7, GP8, GP14 }
+// #define MATRIX_COL_PINS { GP4, GP3, GP2, GP1, GP0, GP15, GP26, GP27, GP28, GP29 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+// 200 is the default
+#define TAPPING_TERM 250
+
+// #define PERMISSIVE_HOLD
+
+// #define QUICK_TAP_TERM 100
