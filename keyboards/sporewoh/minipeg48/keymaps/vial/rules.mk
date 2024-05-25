b7d77bf53b3a98b0fe26a2120c3e9fd2468f3f2b
tvizninja <108183744+tvizninja@users.noreply.github.com>
2024-05-25 18:26:24 +0000

    add key override

--- keyboards/sporewoh/minipeg48/keymaps/vial/rules.mk
+++ keyboards/sporewoh/minipeg48/keymaps/vial/rules.mk
@@ -2,3 +2,6 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 LTO_ENABLE = yes
 QMK_SETTINGS = no
+
+include keyboards/sporewoh/minipeg48/keymaps/vial/key_override/rules.mk
+VPATH += keyboards/sporewoh/minipeg48/keymaps/vial/key_override
