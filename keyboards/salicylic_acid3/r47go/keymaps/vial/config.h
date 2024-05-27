784243c766a4a147acd99160ef6b3ffb1afd7732
tvizninja <108183744+tvizninja@users.noreply.github.com>
2024-05-27 16:23:39 +0000

    add r47go

---
+++ keyboards/salicylic_acid3/r47go/keymaps/vial/config.h
@@ -0,0 +1,46 @@
+/* Copyright 2021 Salicylic_acid3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Select hand configuration */
+
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 180
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define HOLD_ON_OTHER_KEY_PRESS
+#define VIAL_KEYBOARD_UID {0x71, 0x6C, 0xE4, 0x1E, 0x36, 0x31, 0xDE, 0xAB}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+
+#define DYNAMIC_MACRO_NO_NESTING
+#define DYNAMIC_MACRO_DELAY 8
+
+#define VIAL_TAP_DANCE_ENTRIES 16
+#define VIAL_COMBO_ENTRIES 12
+#define VIAL_KEY_OVERRIDE_ENTRIES 20
+
+#define MOUSEKEY_DELAY 10
+#define MOUSEKEY_INTERVAL 20
+#define MOUSEKEY_MOVE_DELTA 2
+#define MOUSEKEY_MAX_SPEED 16
+#define MOUSEKEY_TIME_TO_MAX 30
+#define MOUSEKEY_WHEEL_DELAY 10
+#define MOUSEKEY_WHEEL_INTERVAL 80
+#define MOUSEKEY_WHEEL_DELTA 1
+#define MOUSEKEY_WHEEL_MAX_SPEED 8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
