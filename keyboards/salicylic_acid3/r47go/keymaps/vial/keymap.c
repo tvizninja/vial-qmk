cc19525a9c4df14a656e20ecbed24108b722b9a4
tvizninja <108183744+tvizninja@users.noreply.github.com>
2024-05-28 17:16:24 +0000

    add jp-us key override

--- keyboards/salicylic_acid3/r47go/keymaps/vial/keymap.c
+++ keyboards/salicylic_acid3/r47go/keymaps/vial/keymap.c
@@ -15,6 +15,19 @@
  */
 #include QMK_KEYBOARD_H
 
+#include "virtser.h"
+#include "eeconfig.h"
+#include "vial.h"
+
+#include "os_key_override.h"
+
+typedef union {
+  uint32_t raw;
+  struct {
+    uint8_t     key_os_override :1;
+  };
+} user_config_t;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINS, KC_DEL,
@@ -41,3 +54,71 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,       _______,        _______,        _______,     _______, _______,  _______,   _______
     )
 };
+
+user_config_t user_config;
+
+void keyboard_post_init_user(void) {
+    os_key_override_init();
+
+    user_config.raw = eeconfig_read_user();
+    switch (user_config.key_os_override) {
+        case KEY_OS_OVERRIDE_DISABLE:
+            remove_all_os_key_overrides();
+            break;
+        case US_KEY_JP_OS_OVERRIDE_DISABLE:
+            register_us_key_on_jp_os_overrides();
+            break;
+    }
+}
+
+#define DEFFERED_KEY_RECORD_LEN 6
+static keyrecord_t deferred_key_record[DEFFERED_KEY_RECORD_LEN];
+
+static void push_deferred_key_record(uint16_t keycode, keyevent_t *event) {
+    for (int i = 0; i < DEFFERED_KEY_RECORD_LEN; i++) {
+        if (deferred_key_record[i].keycode == KC_NO) {
+            keyrecord_t record     = {.event = *event, .keycode = keycode};
+            deferred_key_record[i] = record;
+            return;
+        }
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // To apply key overrides to keycodes combined shift modifier, separate to two actions
+    if (keycode >= QK_MODS && keycode <= QK_MODS_MAX) {
+        if (record->event.pressed) {
+            register_mods(QK_MODS_GET_MODS(keycode));
+            uint16_t   deferred_keycode   = QK_MODS_GET_BASIC_KEYCODE(keycode);
+            keyevent_t deferred_key_event = (keyevent_t){.type = KEY_EVENT, .key = (keypos_t){.row = VIAL_MATRIX_MAGIC, .col = VIAL_MATRIX_MAGIC}, .pressed = 1, .time = (timer_read() | 1)};
+            push_deferred_key_record(deferred_keycode, &deferred_key_event);
+        } else {
+            uint16_t   deferred_keycode   = QK_MODS_GET_BASIC_KEYCODE(keycode);
+            keyevent_t deferred_key_event = ((keyevent_t){.type = KEY_EVENT, .key = (keypos_t){.row = VIAL_MATRIX_MAGIC, .col = VIAL_MATRIX_MAGIC}, .pressed = 0, .time = (timer_read() | 1)});
+            unregister_mods(QK_MODS_GET_MODS(keycode));
+            push_deferred_key_record(deferred_keycode, &deferred_key_event);
+        }
+        return false;
+    }
+
+    if (record->event.pressed) {
+        switch (keycode) {
+            case QK_KB_0:
+            remove_all_os_key_overrides();
+            user_config.key_os_override = KEY_OS_OVERRIDE_DISABLE;
+            eeconfig_update_user(user_config.raw);
+            return false;
+
+            case QK_KB_1:
+            register_us_key_on_jp_os_overrides();
+            user_config.key_os_override = US_KEY_JP_OS_OVERRIDE_DISABLE;
+            eeconfig_update_user(user_config.raw);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void post_process_record_user(uint16_t keycode, keyrecord_t* record) {
+}
--- keyboards/salicylic_acid3/r47go/keymaps/vial/rules.mk
+++ keyboards/salicylic_acid3/r47go/keymaps/vial/rules.mk
@@ -3,4 +3,7 @@ VIAL_ENABLE = yes
 LTO_ENABLE = yes
 QMK_SETTINGS = no
 
-#DYNAMIC_MACRO_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = no
+
+include keyboards/salicylic_acid3/r47go/keymaps/vial/key_override/rules.mk
+VPATH += keyboards/salicylic_acid3/r47go/keymaps/vial/key_override
