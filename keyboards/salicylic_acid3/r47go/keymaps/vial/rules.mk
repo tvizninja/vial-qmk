bc0c9b85c3be3fd831c3271f2ddd6b7d1ec23113
tvizninja <108183744+tvizninja@users.noreply.github.com>
2024-05-29 14:57:53 +0000

    cfw-18-12-18

--- keyboards/salicylic_acid3/r47go/keymaps/vial/rules.mk
+++ keyboards/salicylic_acid3/r47go/keymaps/vial/rules.mk
@@ -5,5 +5,5 @@ QMK_SETTINGS = no
 
 DYNAMIC_MACRO_ENABLE = no
 
-include keyboards/salicylic_acid3/r47go/keymaps/vial/key_override/rules.mk
-VPATH += keyboards/salicylic_acid3/r47go/keymaps/vial/key_override
+#include keyboards/salicylic_acid3/r47go/keymaps/vial/key_override/rules.mk
+#VPATH += keyboards/salicylic_acid3/r47go/keymaps/vial/key_override
